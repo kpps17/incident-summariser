2a85c9384d6bfab222f0deda0fe80b1ea8f2d7a0
Ayush Kumar <43320248+CrypticCoder2502@users.noreply.github.com>
2023-04-20 21:08:42 -0700

    cosmetic changes
    Made the cosmetic changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import {TicketDetailComponent} from "./Components/TicketDetailsComponent/TicketDetailsComponent";
 import UserChatComponent from "./Components/UserChatComponent/UserChatComponent";
 import './App.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+
 
 function App() {
   return (
     <div className="App">
         <div className="incident-summariser-header">
             <span>
-                Hello from incident summariser
+                Incident Insight
             </span>
         </div>
         <div className="incident-summariser-required-component">
